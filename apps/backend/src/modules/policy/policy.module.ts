9dc8772ed4a4941c4e7bb710c3f0dc18587d68a2
Gowtham K <gkukkada@kent.edu>
2026-02-16 16:41:41 -0500

    fixed railway error

--- apps/backend/src/modules/policy/policy.module.ts
+++ apps/backend/src/modules/policy/policy.module.ts
@@ -3,9 +3,10 @@ import { PolicyService } from './policy.service';
 import { PolicyController } from './policy.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from '../auth/auth.module';
+import { OrganizationModule } from '../organization/organization.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [PrismaModule, OrganizationModule, AuthModule],
   controllers: [PolicyController],
   providers: [PolicyService],
   exports: [PolicyService],
